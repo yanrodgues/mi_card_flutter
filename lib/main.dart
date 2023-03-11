62405b3b66a87acc7b9c47296e83cc2e2d4adef5
cabelo de bíblia <yandeassis@pm.me>
2023-03-11 20:11:16 -0300

    basic cards

--- lib/main.dart
+++ lib/main.dart
@@ -9,37 +9,82 @@ class MeuApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.teal.shade300,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: <Widget>[
-              Container(
-                width: 100.0,
-                height: double.infinity,
-                color: Colors.red,
+              SizedBox(height: 15.0),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                foregroundImage: AssetImage('images/avatar.png'),
               ),
-              Container(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+              Text(
+                'Yan',
+                style: TextStyle(
+                    fontSize: 50.0,
+                    color: Colors.white,
+                    fontFamily: 'Merienda'),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSans3',
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.lightBlueAccent[100]),
+              ),
+              Card( //container for the phone number
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical:10.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Row(
                     children: <Widget>[
-                      Container(
-                        color: Colors.yellow,
-                        width: 100.0,
-                        height: 100.0,
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal.shade900,
+                        size: 50,
                       ),
-                      Container(
-                        color: Colors.green,
-                        width: 100.0,
-                        height: 100.0,
+                      SizedBox( width: 10.0),
+                      Text(
+                        '35 9 8866 0210',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSans3',
+                          fontSize: 20.0,
+                        ),
+                      )
+                    ]
+                    ),
+                ),
+              ),
+              Card( //container for the email address
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical:10.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal.shade900,
+                        size: 50,
                       ),
-                    ]),
-              ),
-              Container(
-                color: Colors.blue,
-                width: 100.0,
-                height: double.infinity,
-              ),
+                      SizedBox( width: 10.0),
+                      Text(
+                        'yandeassis@pm.me',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSans3',
+                          fontSize: 20.0,
+                        ),
+                      )
+                    ]
+                    ),
+                ),
+              )
             ],
           ),
         ),
