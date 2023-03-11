90f76042060ba310955ff43c7b48518aa38b9eb1
cabelo de bíblia <yandeassis@pm.me>
2023-03-10 21:25:39 -0300

    Flutter Layouts Challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                height: double.infinity,
+                color: Colors.red,
+              ),
+              Container(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.yellow,
+                        width: 100.0,
+                        height: 100.0,
+                      ),
+                      Container(
+                        color: Colors.green,
+                        width: 100.0,
+                        height: 100.0,
+                      ),
+                    ]),
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100.0,
+                height: double.infinity,
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
